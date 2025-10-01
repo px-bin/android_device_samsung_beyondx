27175faf6b1a3e6102484ef098c51de860d4d768
pjgowtham <pjgowtham@gmail.com>
2025-10-01 13:52:49 +0300

    beyondx: displayconfig: Setup density mapping
    Change-Id: I1a0cab5d554b072d889133128b49f8ccf4d4fb21
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device.mk
+++ device.mk
@@ -27,11 +27,17 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+
 # Disable Matlog
 TARGET_DISABLE_MATLOG := true
 
 #Sign Builds
 -include vendor/lineage-priv/keys/keys.mk
+
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/display_id_4633128672291735937.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4633128672291735937.xml
+
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
